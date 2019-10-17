d42fae4540d3dc689a4007258f3863dbce1a536e
zhaoye <yue.joo@gmail.com>
2019-10-17 20:08:21 +0800

    Initial Commit

---
+++ YJRadarChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YJRadarChart
+//
+//  Created by zhaoye on 10/17/19.
+//  Copyright © 2019 zhaoye. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
